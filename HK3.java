97a6f27736f1833d9f54047f0eb7e45800d6aaf8
Rohit pandey <61056082+Rohit484@users.noreply.github.com>
2021-10-02 11:56:08 +0530

    Add files via upload

---
+++ HK3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ROHIT PANDEY
+ */
+import java.util.*;
+public class HK3 {
+    public static void main(String[]args)
+    {
+        int no,s;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        no=sc.nextInt();
+        if(no%2==0 && no<5)
+            System.out.println("Not warid");
+        else
+    }
+    
+}
